3e5f282243cec030833d667ab719eebe7cb25251
EBLANCO17 <emersonblanco2015@gmail.com>
2021-09-12 18:42:42 -0500

    Se agregan mas campos a la tabla

--- src/app/pages/tabla/tabla.component.ts
+++ src/app/pages/tabla/tabla.component.ts
@@ -25,5 +25,10 @@ const ELEMENT_DATA: PeriodicElement[] = [
   {codigo: 1, nombre: 'Pasta', categoria: 'Alimento', cantidad: 30},
   {codigo: 2, nombre: 'Cuchara', categoria: 'Hogar', cantidad: 20}, 
   {codigo: 3, nombre: 'Camisa', categoria: 'Ropa', cantidad: 19},
-  {codigo: 4, nombre: 'Manzanas', categoria: 'Alimento', cantidad: 40}   
+  {codigo: 4, nombre: 'Manzana', categoria: 'Alimento', cantidad: 40}, 
+  {codigo: 5, nombre: 'Tenis', categoria: 'Calzado', cantidad: 7}, 
+  {codigo: 6, nombre: 'Balón', categoria: 'Deportes', cantidad: 21}, 
+  {codigo: 7, nombre: 'Computador', categoria: 'Tecnologia', cantidad: 70}, 
+  {codigo: 8, nombre: 'Jeans', categoria: 'Ropa', cantidad: 56}, 
+  {codigo: 9, nombre: 'Pera', categoria: 'Alimento', cantidad: 40}   
 ];
